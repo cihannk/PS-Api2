1ecb4dbbb4b939ec8995c5eab2be8ab73b94d2c1
Cihan <Cihan@DESKTOP-G0QE5OI>
2022-06-21 19:27:18 +0300

    Add project files.

---
+++ PS-Api2/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PS_Api2.Controllers
+{
+    public class HomeController : ControllerBase
+    {
+        private readonly int _readinessLatencyInSec, _startupLatencyInSec;
+        public HomeController(IConfiguration configuration)
+        {
+            _readinessLatencyInSec = configuration.GetSection("Timeouts").GetValue<int>("readinessLatencyInSec");
+            _startupLatencyInSec = configuration.GetSection("Timeouts").GetValue<int>("startupLatencyInSec");
+        }
+
+        [HttpGet("ping")]
+        public IActionResult Ping()
+        {
+            return Ok("pong");
+        }
+
+        [HttpGet("readiness")]
+        public IActionResult Readiness()
+        {
+            Thread.Sleep(_readinessLatencyInSec * 1000);
+            return Ok();
+        }
+
+        [HttpGet("liveness")]
+        public IActionResult Liveness()
+        {
+            return Ok();
+        }
+
+        [HttpGet("startup")]
+        public IActionResult Startup()
+        {
+            Thread.Sleep(_startupLatencyInSec * 1000);
+            return Ok();
+        }
+
+        [HttpGet("getsettings")]
+        public IActionResult Settings()
+        {
+            return Ok(new
+            {
+                ReadinessLatencyInSeconds = _readinessLatencyInSec,
+                StartupLatencyInSeconds = _startupLatencyInSec
+            }
+            );
+        }
+
+
+    }
+}
